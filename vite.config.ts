c1bac5d85b82ac6c77ab572c359820782faeb5f9
Swarupanand05 <spb05092004@gmail.com.com>
2026-08-11 16:14:07 +0530

    ci: add automated deployment for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/IOT-Project/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
